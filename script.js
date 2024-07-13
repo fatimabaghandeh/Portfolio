20f1ee97359f685f0d1abac317fab001115bfe4b
Fatima.Baghandeh <fatima.baghandeh@gmail.com>
2024-07-13 16:04:53 +0330

    added mode switcher

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ List.style.maxHeight= '0px';
 
 function toggleMenu(){
     if(List.style.maxHeight == '0px'){
-        List.style.maxHeight = '300px'
+        List.style.maxHeight = '400px'
     }
     else{
         List.style.maxHeight = '0px'
     }
 }
+
+
+
+const switchTheme = () => {
+    const root = document.documentElement;
+    let dataTheme = root.getAttribute("data-theme"),
+    newTheme = (dataTheme === "light") ? "dark" : "light";
+    root.setAttribute("data-theme", newTheme);
+}
+
+document.querySelector("#theme-switcher").addEventListener("click", switchTheme)
+
+  
